5cee97c5c34b0ca007677adadf3c47c3f9b4de39
Just1truc <justin.duc@epitech.eu>
2022-05-01 17:02:28 +0200

    [UPDATE] Adding check on node modification and remove constant check

--- main_blocker.js
+++ main_blocker.js
@@ -1,4 +1,5 @@
+document.querySelectorAll("div[class*='ads'], div[class*='ad_native'], div[class='ad'], div[id='ad'], div[id*='ad_native'], div[id*='ads'], div[id*='widget']").forEach((item) => {item.style.display = 'none'});
 
-setInterval(()=> {
+document.addEventListener("DOMNodeInserted", function () {
     document.querySelectorAll("div[class*='ads'], div[class*='ad_native'], div[class='ad'], div[id='ad'], div[id*='ad_native'], div[id*='ads'], div[id*='widget']").forEach((item) => {item.style.display = 'none'});
-},100);
+}, false);
